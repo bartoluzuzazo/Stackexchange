5e9c747aa0d0d2749707370f4f2b1794f6a1f9d9
bartoluzuzazo <bartbo62@gmail.com>
2024-03-28 12:17:56 +0100

    init

---
+++ Stackexchange.API/Controllers/TagController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Stackexchange.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TagController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetOk()
+    {
+        return Ok();
+    }
+}
