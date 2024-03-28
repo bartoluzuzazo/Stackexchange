1526319927474c0a871a1968304fdcf233401b50
bartoluzuzazo <bartbo62@gmail.com>
2024-03-28 13:30:36 +0100

    added Tag model

---
+++ Stackexchange.Domain/Tag/Tag.cs
@@ -0,0 +1,11 @@
+﻿namespace Stackexchange.Domain.Tag;
+
+public class Tag
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public int Count { get; set; }
+    public bool IsRequired { get; set; }
+    public bool IsModeratorOnly { get; set; }
+    public bool HasSynonyms { get; set; }
+}
